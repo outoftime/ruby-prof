2d91d41856733d694959435d0a5c0ac736b33833
John Crepezzi <john.crepezzi@gmail.com>
2013-06-07 17:32:27 -0400

    fix base %

--- lib/ruby-prof/printers/json_stack_printer.rb
+++ lib/ruby-prof/printers/json_stack_printer.rb
@@ -18,7 +18,7 @@ module RubyProf
 
     # Encode data from all threads and then print
     def print(output = STDOUT, options = {})
-      data = { label: '(root)', children: [], percentage: 1 }
+      data = { label: '(root)', children: [], percentage: 100 }
       @total_time = @result.threads.inject(0) { |val, thread| val += thread.total_time }.to_f
       @result.threads.each do |thread|
         record_thread(thread, data[:children])
